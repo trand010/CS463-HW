c9010866cee6c6a97d5cbbc9f151abcf98c768fb
Daniel Tran <danieltran219@gmail.com>
2023-10-08 22:10:24 -0700

    completing hw1

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -1,5 +1,11 @@
-window.addEventListener('load', loadPage);
+window.addEventListener("load", loadPage);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
+  console.log("The page has now loaded.");
+  console.log(
+    "My name is Daniel and I am a student in CS463: Intro to Web " +
+      "Development this quarter. I took this class to learn more about HTML, " +
+      "CSS, JavaScript, and to overall gain the foundamental skills of a web " +
+      "developer."
+  );
 }
